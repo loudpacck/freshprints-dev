a4d1ce4fc756bbe18782d7c6d0ad031bfea17127
LOUDD <kyledebord118@gmail.com>
2026-05-30 12:21:53 -0400

    feat: add Pray by LOUDDAI

--- src/data/beatBeatersCharts.js
+++ src/data/beatBeatersCharts.js
@@ -1,5 +1,15 @@
 // Add new songs here after dropping chart JSON into
 // public/charts/ and audio into public/audio/
 
-
-export const BEAT_BEATERS_CHARTS = [];
+export const BEAT_BEATERS_CHARTS = [
+  {
+    id: 'pray',
+    title: 'Pray',
+    artist: 'LOUDDAI',
+    bpm: 85,
+    audioFile: 'Pray.mp3',
+    chartFile: 'louddai-pray-medium.json',
+    availableDifficulties: ['medium'],
+    accentColor: '#faac04',
+  },
+];
